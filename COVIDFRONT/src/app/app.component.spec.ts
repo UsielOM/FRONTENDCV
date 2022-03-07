05313cacfb9f6044c4c105e26c9863debf3eeca9
UsielOM <90886892+UsielOM@users.noreply.github.com>
2022-03-07 11:36:46 -0600

    Dependencias instaladas
    PDF MAKE
    PDF FONT
    NG CHAR

--- COVIDFRONT/src/app/app.component.spec.ts
+++ COVIDFRONT/src/app/app.component.spec.ts
@@ -1,13 +1,9 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
         AppComponent
       ],
@@ -20,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'COVIDFRONT'`, () => {
+  it(`should have as title 'Teleconsulta-SPCovid19'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('COVIDFRONT');
+    expect(app.title).toEqual('Teleconsulta-SPCovid19');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('COVIDFRONT app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Teleconsulta-SPCovid19 app is running!');
   });
 });
